0fd90d9dc7ea1e0951dd0841cbbd700bec3bc205
gightkingss <gightkingss@gmail.com>
2023-08-09 22:34:36 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main-A program to output a line
+ * Return: 0 (Success)
+ */
 int main(void) {
     puts("\"Programming is like building a multilingual puzzle");
     return 0;
