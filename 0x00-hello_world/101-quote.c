dd8bb53945ffd94edccc3c87b767d71182fefefa
gightkingss <gightkingss@gmail.com>
2023-08-10 19:51:23 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,11 @@
+#include <stdio.h>
 #include <unistd.h>
-
-int main() {
-    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, sizeof(message) - 1); // Using file descriptor 2 for standard error
-    return 1;
+/**
+ * main- A program to output in the stardard error
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
