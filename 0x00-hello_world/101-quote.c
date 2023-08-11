e9f2a6c3e050e295c460a407bc2ba5987cabc157
gightkingss <gightkingss@gmail.com>
2023-08-10 19:43:05 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,8 @@
+#!/bin/bash
+#include <unistd.h>
+
+int main() {
+    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, message, sizeof(message) - 1); // Using file descriptor 2 for standard error
+    return 1;
+}
