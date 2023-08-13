7f75b635ea2f9496085a664693962849b7f2d820
gightkingss <gightkingss@gmail.com>
2023-08-13 09:22:00 -0700

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,11 +10,11 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n>0){
-printf("%d\t is positive",n);
+printf("%d is positive",n);
 }else if(n==0){
-printf("%d\t is zero",n);
+printf("%d is zero",n);
 }else{
-printf("%d\t is negative",n);
+printf("%d is negative",n);
 }
 return 0;
 }
