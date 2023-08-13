7cab542b2114a898410f92a0f959bb1505ac8cc3
gightkingss <gightkingss@gmail.com>
2023-08-13 14:52:54 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ else if (abs(n%10) == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, abs(n%10));
 }
-else if (abs(n%10) != 0 && abs(n%10) < 6)
+else
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n%10);
 }
