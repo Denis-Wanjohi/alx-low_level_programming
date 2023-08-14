0d3634b38c943376e32b7cd6cafbac77bd9e3a9d
gightkingss <gightkingss@gmail.com>
2023-08-14 01:18:03 -0700

    alphabets

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-int main (void)
+/**
+ * main - Prints the alphabet skipping 'q' and 'e'.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 char ch;
-for (ch = 'a';ch <= 'z';++ch)
+for (ch = 'a'; ch <= 'z'; ++ch)
 {
 if (ch == 'q' || ch == 'e')
 {
