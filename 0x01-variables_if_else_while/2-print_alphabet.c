0607c915e5aeb32766292d75e9e92208a0b72bd9
gightkingss <gightkingss@gmail.com>
2023-08-13 21:41:07 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*main-runs the function
+ *
+ * the code prinys alphabets in samll letters
+ *
+ * return (0) success
+*/
+int main()
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+putchar(letter);
+letter ++;
+}
+putchar(\n);
+return (0);
+}
