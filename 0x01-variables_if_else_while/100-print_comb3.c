a8201ad040a64ad22df5350729b1129fd8b82313
gightkingss <gightkingss@gmail.com>
2023-08-14 19:54:16 -0700

    combinations of two numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,10 +4,12 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
-for (int i = 0; i < 10; i++)
+int main(void)
 {
-for (int j = i + 1; j < 10; j++)
+int i,j;
+for (i = 0; i < 10; i++)
+{
+for (j = i + 1; j < 10; j++)
 {
 putchar(i + '0');
 putchar(j + '0');
