7f70204b3c4c08e9779f7935f3273a5cdf1045f9
gightkingss <gightkingss@gmail.com>
2023-08-16 13:12:32 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the int to check
+ * Return: the absolute value of int
+ */
+int _abs(int x)
+{
+printf(abs(int x));
+return (0);
+}
