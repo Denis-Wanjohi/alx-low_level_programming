32c74cef136fd5c1e9c80f6c325493c1cfcde67b
gightkingss <gightkingss@gmail.com>
2023-08-15 20:40:35 -0700

    10  times print

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabet in lowercase 10 times
+ */
+void print_alphabet_x10(void)
+{
+int x = 10;
+while (x <= 10)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+_putchar('\n');
+}
+}
