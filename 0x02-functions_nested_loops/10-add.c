8d99c92d24fccd06652c40771b5349eabd80beab
gightkingss <gightkingss@gmail.com>
2023-08-16 19:28:06 -0700

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * add-add two int and returnthe value;
- * @x-value to be used
- * @y-value to be used
- * Return - gives back the sum
+ * @x-int to be added to y
+ * @y-int to be added to x
+ * Return: sum of a and b
  */
 
 int add(int x, int y)
