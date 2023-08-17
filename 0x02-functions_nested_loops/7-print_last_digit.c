78fd5fee44fa1fdcca355bc6cafb1fc719c1cb0c
gightkingss <gightkingss@gmail.com>
2023-08-16 17:02:58 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,12 @@
 #include "main.h"
 /**
- *int print_last_digit(int)- prints the last digit
+ * print_last_digit- prints the last digit
+ * @x - number to be checked
+ * returns the value of the last digit
  */
 
 int print_last_digit(int x)
 {
+_putchar(x%10);
 return (x%10);
 }
