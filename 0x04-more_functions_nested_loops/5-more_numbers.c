0a15528c148cfead954200d700515bd2b25eafc0
gightkingss <gightkingss@gmail.com>
2023-08-17 03:55:31 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - prints from 0 -14
+ *
+ *
+ */
+void more_numbers(void)
+{
+int x;
+for (x = 0; x <=10; x++)
+{
+int y;
+for (y =0; y <= 14; y++)
+{
+_putchar(y);
+}
+_putchar('\n');
+}
+}
