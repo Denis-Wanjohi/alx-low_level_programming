1c34e0282331167b9c2f39008ef2bc623dbc3901
gightkingss <gightkingss@gmail.com>
2023-08-21 14:06:42 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_array:display array content
+ * @a: the array
+ * @n : size of the arrya
+ */
+void print_array(int *a, int n)
+{
+int x;
+
+for (x = 0; x >= n; x++)
+{
+printf(a[x]);
+}
+}
