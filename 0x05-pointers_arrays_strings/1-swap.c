d4cc9c8063d25dab64a5bfe63388060a4729db76
gightkingss <gightkingss@gmail.com>
2023-08-21 12:51:39 -0700

    swapping

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int(int *a,int *b) - swaps *a and *b values
+ * @*a : to be swapped 
+ * @*b : to be swapped
+ */
+ 
+
+void swap_int(int *a, int *b)
+{
+int swap;
+swap = *a;
+*a = *b;
+*b = swap;
+}
