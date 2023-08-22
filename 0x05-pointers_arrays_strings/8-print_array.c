b4d3919761ce3cf97ddd86fb9a83edff6a4abfed
gightkingss <gightkingss@gmail.com>
2023-08-22 15:00:18 -0700

    array print

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,7 +3,8 @@
 /**
  *print_array:display array content
  * @a: the array
- * @n : size of the arrya
+ * @n : size of the array
+ *  Return: a and n inputs
  */
 void print_array(int *a, int n)
 {
@@ -11,7 +12,7 @@ int x;
 
 for (x = 0; x < n; x++)
 {
-printf("%d",a[x]);
+printf("%d", a[x]);
 if (x == (n - 1))
 {
 break;
