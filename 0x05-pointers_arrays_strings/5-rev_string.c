03deeb9f2e6d4b517605da0972fce6367f227ecc
gightkingss <gightkingss@gmail.com>
2023-08-21 22:14:20 -0700

    string rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * rev_string- funcyion toprint in reverse
+ * @s: strimg to be used
+ */
+
+
+void rev_string(char *s)
+{
+
+int x;
+x = strlen(s);
+int y;
+for (y = (strlen(s))-1;  y >= 0; y--)
+{
+_putchar(s[y]}
+}
+}
