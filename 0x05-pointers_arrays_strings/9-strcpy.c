8d7fede6c264ac535150f773672031f81b8751ea
gightkingss <gightkingss@gmail.com>
2023-08-22 15:12:56 -0700

    string copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <string.h>
-
+/**
+ * *_strcpy(char *dest, char *src)- copies content
+ * @dest: destination
+ * @src: source
+ * Return: destination value
+ */
 
 
 char *_strcpy(char *dest, char *src)
