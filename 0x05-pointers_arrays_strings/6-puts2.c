96526dbd0c09849b65e5b356c220a588736193c6
gightkingss <gightkingss@gmail.com>
2023-08-22 13:03:11 -0700

    puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,13 +1,17 @@
 #include "main.h"
 #include <string.h>
+/**
+ * puts2(char *str)-prints a string halfway
+ * @str:string being used
+ */
 void puts2(char *str)
 {
 
 int len = strlen(str);
 int x;
-for ( x = 0; x < len; x++)
+for (x = 0; x < len; x++)
 {
-if( x%2 == 0)
+if (x % 2 == 0)
 {
 _putchar(str[x]);
 }
