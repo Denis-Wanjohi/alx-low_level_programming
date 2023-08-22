96e1995d942f21edb3dea7ba237fffdb72a78252
gightkingss <gightkingss@gmail.com>
2023-08-22 13:29:53 -0700

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+#include <string.h>
+/**
+ *puts2(char *str)-prints half of the string
+ *@str:string being used 
+*/
+void puts2(char *str)
+{
+int x;
+int len=strlen(str)
+for (x = (len)/2; x < len; x++)
+{
+putchar(str[x]);
+}
+}
