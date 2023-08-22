c39b822129da6213b03b074b7f67a36e6237965c
gightkingss <gightkingss@gmail.com>
2023-08-22 15:01:49 -0700

    array print

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *print_array:display array content
- * @a: the array
- * @n : size of the array
- *  Return: a and n inputs
+ * print_array - a function that prints n elements of an array
+ * @a: array name
+ * @n: is the number of elements OF the array to be printed
+ * Return: a and n inputs
  */
 void print_array(int *a, int n)
 {
