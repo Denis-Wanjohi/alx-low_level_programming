5eb33b57d2cd30a420c80f0954da2900a673f545
gightkingss <gightkingss@gmail.com>
2023-08-22 11:51:11 -0700

    string rev

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,10 +8,17 @@
 
 void rev_string(char *s)
 {
-int y;
-for (y = (strlen(s))-1;  y >= 0; y--)
-{
-_putchar(s[y]);
-}
-_putchar('\n');
+	char rev = s[0];
+	int counter = 0;
+	int i;
+
+	while (s[counter] != '\0')
+	counter++;
+	for (i = 0; i < counter; i++)
+	{
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
+	}
 }
