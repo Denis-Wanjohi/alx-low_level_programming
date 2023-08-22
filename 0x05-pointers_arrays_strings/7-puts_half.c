27296850296eb94a77bba98cb563f26a6822f5cb
gightkingss <gightkingss@gmail.com>
2023-08-22 14:15:19 -0700

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,18 +1,17 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <string.h>
 /**
  *puts_half(char *str)-prints half of the string
- *@str:string being used 
+ *@str:string being used
 */
 void puts_half(char *str)
 {
 int x;
 int len = strlen(str);
 int half = len / 2;
-if(len % 2 == 1){
-  half = (len - 1) / 2;
-}
-for (x = half; x < len; x++)
+if (len % 2 == 1)
+half = (len - 1) / 2;
+for (x = half + 1; x < len; x++)
 {
 putchar(str[x]);
 }
