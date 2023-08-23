9ce64087d15aa4ccca66a54c6c407d19b538d792
gightkingss <gightkingss@gmail.com>
2023-08-23 08:10:36 -0700

    string concatenated

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strncat(char *dest, char *src, int n)-function that combines string with condition
+ * @dest:string being used
+ * @src:string being used
+ * Return: the concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+char src1=src[n];
+int x=strlen(dest);
+dest[x]=src1;
+return (dest);
+}
