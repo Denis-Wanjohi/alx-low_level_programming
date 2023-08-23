dd075d23c6670c76c43b9ffd3a0b018011231342
gightkingss <gightkingss@gmail.com>
2023-08-23 08:44:32 -0700

    comparison

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * int _strcmp(char *s1, char *s2)
+ * @s1:string to compare to s2
+ * @s2: string to compare to s1
+ * Return-return the comparison
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+return(strcmp(s1,s2));
+}
