ac93dade7d4c515cd4a754455baff6b6236f115d
gightkingss <gightkingss@gmail.com>
2023-08-23 08:31:39 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strncpy(char *dest, char *src, int n)-performs copying
+ * @dest: string being used
+ * @src: string being used
+ * @n: index
+ * Return : final string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+
+
+}
