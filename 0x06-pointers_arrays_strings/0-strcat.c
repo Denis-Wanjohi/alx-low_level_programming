b46fe95d4ec8631334e5047ea6758375d20b1a90
gightkingss <gightkingss@gmail.com>
2023-08-23 07:37:08 -0700

    concatenated

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcat(char *dest, char *src)- function that combines two strings
+ * @dest: string beibg used
+ * @src: string being used
+ * Return - concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+return (strcat(dest,src));
+}
