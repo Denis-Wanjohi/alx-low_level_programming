480e211a6512f4f51b1f7f4289ef08d730a8569a
gightkingss <gightkingss@gmail.com>
2023-08-24 19:41:20 -0700

    comparison

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,5 +9,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-return(strcmp(s1,s2));
+int i;
+i = 0;
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+if (s1[i] != s2[i])
+{
+return (s1[i] - s2[i]);
+}
+i++;
+}
+return (0);
 }
