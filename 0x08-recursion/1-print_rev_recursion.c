57f5b8ed270df6c7c7923e0868b48b59b6bcb890
gightkingss <gightkingss@gmail.com>
2023-08-30 12:42:09 -0700

    reverse form

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * void _print_rev_recursion(char *s)-prints in reverse
+ * @s:string being used;
+ */
+
+void _print_rev_recursion(char *s)
+{
+if(*s)
+{
+_print_rev_recursion(s+1);
+putchar(*s);
+}
+}
