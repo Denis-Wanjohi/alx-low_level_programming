8a8a8e7da47b04e9a43513c8993eb783d3756ca6
gightkingss <gightkingss@gmail.com>
2023-08-30 13:43:12 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * factorial (int n)-gets the factorial
+ * @n:the value;
+ * Return: value if positive and -1 if n is -ve
+ */
+
+int factorial (int n)
+{
+if (n > 0){
+n*=factorial (n-1);
+}
+else if (n == 0)
+{
+return (1);
+}
+else
+{
+return (-1);
+}
+return (n);
+}
