31fd87d99b35bc77eee0efb8af0afde3c0f41c43
gightkingss <gightkingss@gmail.com>
2023-08-30 10:00:36 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts_half(char *str);
-char *_strcpy(char *dest, char *src);
-#endif
