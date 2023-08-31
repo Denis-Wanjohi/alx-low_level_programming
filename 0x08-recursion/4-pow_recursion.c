0c8ddba480773a9e779e37012c588dc390637b61
gightkingss <gightkingss@gmail.com>
2023-08-30 22:32:38 -0700

    power to

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * __pow_recursion(int x, int y)-calculates x powered to y
+ * _pow_recursion(int x, int y)-calculates x powered to y
  * @x:value
  * @y:power value
- * Return-the powered value
+ * Return:the powered value
  */
 
 
@@ -11,11 +11,11 @@ int _pow_recursion(int x, int y)
 {
 if (y < 0)
 {
-return -1;
+return (-1);
 }
 if (y == 0)
 {
-return 1;
+return (1);
 }
-return (x * _pow_recursion(x,y-1));
+return (x * _pow_recursion(x, y - 1));
 }
