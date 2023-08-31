0e273b0fc4cb7a9500d510dd0bf2d2c15334fe62
gightkingss <gightkingss@gmail.com>
2023-08-31 15:59:22 -0700

    prime numbers

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+int xValue(int n, int u)
+{
+if ((u * u) != n)
+{
+if ((u * u) > n)
+{
+if ((n) % 2 == 0 || (n) % 3 == 0 || (n) % 5 == 0)
+{
+return (0);
+}
+else
+{
+return (1);
+}
+}
+return xValue(n, u + 1);
+}
+else
+{
+if ((n) % 2 == 0 || (n) % 3 == 0 || (n) % 5 == 0)
+{
+return 0;
+}
+else
+{
+return 1;
+}
+}
+}
+int is_prime_number(int n)
+{
+if (n < 0)
+{
+return (0);
+} 
+int z=xValue(n,0);
+return (z);
+}
