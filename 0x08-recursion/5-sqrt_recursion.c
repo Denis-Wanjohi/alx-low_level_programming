e6d0f9ea858399f328524de4554776d0d0db9216
gightkingss <gightkingss@gmail.com>
2023-08-31 15:13:09 -0700

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,20 +6,27 @@
  * Return:the squrae root value
  *
  */
+int xValue(int n,int u)
+{
+if ((u * u) != n)
+{
+if((u*u) > n)
+{
+return (-1);
+}   
+return xValue(n, u + 1);   
+}
+if ((u*u) == n)
+{  
+return (u);
+}
+}
 int _sqrt_recursion(int n)
 {
-int x = 0;
 if (n < 0)
 {
 return (-1);
-}
-while ((x * x) != (n))
-{
-if ((x * x) > n)
-{
-return (-1);
-}
-x++;
-}
-return (x);
+} 
+int z=xValue(n,0);    
+return (z);
 }
