2dc8639f9b16695675c44611b0f534722114d6d2
gightkingss <gightkingss@gmail.com>
2023-09-03 14:06:58 -0700

    my name

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,9 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts_half(char *str);
-char *_strcpy(char *dest, char *src);
+int main(int argc, char *argv[]);
 #endif
