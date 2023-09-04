746ec9f78b839a6e2045169796f9bd94e9b5105f
gightkingss <gightkingss@gmail.com>
2023-09-04 13:00:20 -0700

    my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include "stdio.h"
 
-
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
-	return (0);
+printf("%s\n",argv[0]);
+_putchar('\n');
+return (0);
 }
