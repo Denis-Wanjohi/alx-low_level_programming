55fee75821a5a76775ac415b6692f377212ea795
gightkingss <gightkingss@gmail.com>
2023-09-04 13:28:20 -0700

    no of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,8 +7,8 @@
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
- int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc - 1);
-	return (0);
+printf("%d\n", argc - 1);
+return (0);
 }
