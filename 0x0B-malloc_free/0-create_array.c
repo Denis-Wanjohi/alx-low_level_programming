c7cb3bc68f427ada28e8d11e6f0c320d77fed9c6
gightkingss <gightkingss@gmail.com>
2023-09-05 11:06:37 -0700

    creating array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * create_array(unsigned int size, char c)-creates an array of size size with characters c
+ * @size:size of the array
+ * @c:character being used
+ * Return:return the array
+ */
+char *create_array(unsigned int size, char c)
+{
+if (size == 0)
+{
+return (0);
+}
+
+}
+
