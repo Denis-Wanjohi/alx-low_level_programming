803d2216c4013bc6c3834ead95a15d7f23f6b36c
gightkingss <gightkingss@gmail.com>
2023-09-05 14:05:18 -0700

    creating an array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,24 +9,20 @@
 
 char *create_array(unsigned int size, char c)
 {
-    unsigned int i;
-   char *char_array;
-    if (size == 0)
-    {
-        return NULL;
-    }
-
-   char_array = (char *)malloc(sizeof(char) * size);
-
-    if (char_array == NULL)
-    {
-        return NULL;
-    }
-    
-    for (i = 0; i < size; i++)
-    {
-        char_array[i] = c;
-    }
-
-    return char_array;
+unsigned int i;
+char *char_array;
+if (size == 0)
+{
+return (NULL);
+}
+char_array = (char *)malloc(sizeof(char) * size);
+if (char_array == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < size; i++)
+{
+char_array[i] = c;
+}
+return (char_array);
 }
