a6983c8f7f3e7b16cae92e999068c30a4fd9376c
gightkingss <gightkingss@gmail.com>
2023-09-05 03:56:13 -0700

    yask

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _abs - computes the absolute value of an integer
+ * @x: the int to check
+ * Return: the absolute value of int
+ */
+int _abs(int x)
+{
+return (abs(x));
+}
---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcpy(char *dest, char *src)- copies content
+ * @dest: destination
+ * @src: source
+ * Return: destination value
+ */
+
+
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest, src);
+return (dest);
+}
