efdaea4f0b9ea7a5c9433069cac9ee6cf829b02c
gightkingss <gightkingss@gmail.com>
2023-09-06 14:31:49 -0700

    multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,11 @@
 #include "stdio.h"
-#include <stdlib.h> 
-
+#include <stdlib.h>
+/**
+  *main-multiply the arguments
+  *@argc:argument counter
+  *@argv:argument vectors
+  *Return:return 0 with no errors
+  */
 int main (int argc, char *argv[])
 {
 if (argc != 3)
@@ -10,7 +15,7 @@ return (1);
 }
 else
 {
-printf("%d\n", atoi(argv[1]) * atoi(argv[2]));  
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 }
 return (0);
 }
