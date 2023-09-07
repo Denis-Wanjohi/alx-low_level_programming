71d50844fd28649b3fc8b4ce18fffb51eda57a0e
gightkingss <gightkingss@gmail.com>
2023-09-07 14:24:50 -0700

    malloc checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * malloc_checked(unsigned int b)-chekes memory allocation 
+ * @b:the size 
+ * 
+ */
 void *malloc_checked(unsigned int b)
 {
 int *ptr=(int *)malloc(b);
