28f92fef277727abfdb62e78d8bf8c78060166d0
gightkingss <gightkingss@gmail.com>
2023-09-07 15:42:36 -0700

    malloc checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,15 +1,16 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * malloc_checked(unsigned int b)-chekes memory allocation 
- * @b:the size 
+ * malloc_checked(unsigned int b)-chekes memory allocation
+ * @b:the size
  * Return: pointer to the allocated memory.
  * if malloc fails, status value is equal to 98.
  */
 void *malloc_checked(unsigned int b)
 {
 char *ptr = malloc(b);
-if(ptr == NULL){
+if (ptr == NULL)
+{
 exit(98);
 }
 return (ptr);
