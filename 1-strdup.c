fd0d3a8c78716cf06c246b167188f3693c290b39
gightkingss <gightkingss@gmail.com>
2023-09-06 17:59:22 -0700

    duplication

--- 1-strdup.c
+++ 1-strdup.c
@@ -2,7 +2,10 @@
 char *_strdupli(char *str)
 {
  int i;	
-char *duplicate=(char *)malloc(sizeof(str)+1);
+ if (str == NULL){
+ return NULL;
+ }
+(char *duplicate=(char *)malloc(sizeof(str)+1);
 printf("%d\n",sizeof(str));
 for(i=0;i<sizeof(str);i++){
     duplicate[i]=str[i];
