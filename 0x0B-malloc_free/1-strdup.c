a51f4928befd876287428468fb858bec7409c018
gightkingss <gightkingss@gmail.com>
2023-09-06 18:10:07 -0700

    duplication

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 char *_strdup(char *str)
 {
+char *duplicate;
 unsigned int i;	
  if (str == NULL){
  return NULL;
  }
-char *duplicate=(char *)malloc(sizeof(str)+1);
+duplicate=(char *)malloc(sizeof(str)+1);
 for(i=0;i<sizeof(str);i++){
     duplicate[i]=str[i];
 }
