91a8747f482b7f3d4b08d15e4e8294acc8b0bff7
gightkingss <gightkingss@gmail.com>
2023-09-06 18:01:51 -0700

    duplication

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+char *_strdupli(char *str)
+{
+ int i;	
+ if (str == NULL){
+ return NULL;
+ }
+(char *duplicate=(char *)malloc(sizeof(str)+1);
+printf("%d\n",sizeof(str));
+for(i=0;i<sizeof(str);i++){
+    duplicate[i]=str[i];
+}
+return duplicate;     
+}
