da2e9b69301ef32cb2ed5bc91f63636ed7938aa0
gightkingss <gightkingss@gmail.com>
2023-09-11 17:57:17 -0700

    function

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+
+/**
+ * @dog:struct
+ * @name:value
+ * @age:value
+ * @owner:value
+ * 
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+struct d
+{
+char *name = name;
+float age = age;
+char *owner = owner;
+}
+;
+}
