976165177676d287fc23ec65742febaedfffe669
gightkingss <gightkingss@gmail.com>
2023-09-13 23:48:35 -0700

    my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
-void print_name(char *name, void (*f)(char *)){
+void print_name(char *name, void (*f)(char *))
+{
 if (name == NULL || f == NULL)
-		return;
-
-	f(name);
+return;
+f(name);
 }
