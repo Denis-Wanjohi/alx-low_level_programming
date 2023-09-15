8b24f7dcdf913380642e5f5f29a4aa8634450c82
gightkingss <gightkingss@gmail.com>
2023-09-15 06:57:54 -0700

    my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,9 @@
 #include "function_pointers.h"
-
+/**
+ * print _name(char *name, void (*f) (char *)-function pointer
+ * @name-value from main code
+ * void (*f)(char *)- pointer
+ */
 void print_name(char *name, void (*f)(char *))
 {
 f(name);
