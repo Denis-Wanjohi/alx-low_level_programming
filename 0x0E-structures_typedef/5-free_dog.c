6e37526242bf50096426b4a550e55af781220b10
gightkingss <gightkingss@gmail.com>
2023-09-18 06:33:11 -0700

    free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib>
+/**
+ * free_dog-frees the values
+ * @d:dog to be freed
+ */
+void free_dog(dog_t *d)
+{
+if (d != NULL )
+{
+free(d->name);
+free(d->age);
+free(d->owner);
+}
+}
