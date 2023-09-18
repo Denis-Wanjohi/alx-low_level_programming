0271a60086e28e462f9b6a97100a4db9b108c7ae
gightkingss <gightkingss@gmail.com>
2023-09-18 00:47:13 -0700

    initializer

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,12 +5,13 @@
  * @name:value
  * @age:value
  * @owner:value
- * 
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (dog != NULL)
 {
 d->name = name;
 d->age = age;
 d->owner = owner;
+}
 }
