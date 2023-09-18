d51bb2d270620c7591636b7fde119bcab3981762
gightkingss <gightkingss@gmail.com>
2023-09-18 03:02:33 -0700

    print details

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,14 +8,24 @@
  */
 void print_dog(struct dog *d)
 {
-if (d->name == NULL || d->owner == NULL)
-{   
-printf("nil");
-}
-else
+if( d == NULL)
 {
-printf("Name: %s\n", d->name);
-printf("Age: %f\n", d->age);
-printf("Owner: %s\n", d->owner);
+
+}else
+{
+if (d->name == NULL)
+{   
+d->name="nil";
+}
+if (d->owner == NULL)
+{   
+d->owner="nil";
+}
+
+printf("Name: %s\n",d->name);
+printf("Age: %f\n",d->age);
+printf("Owner: %s\n",d->owner);
 }
 }
+
+
