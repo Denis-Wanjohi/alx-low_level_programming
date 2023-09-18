2a413883afebdfcad22fa0d4a845718555208f95
gightkingss <gightkingss@gmail.com>
2023-09-18 02:49:10 -0700

    print details

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,8 +14,8 @@ printf("nil");
 }
 else
 {
-printf("Name: %s\n",d->name);
-printf("Age: %f\n",d->age);
-printf("Owner: %s\n",d->owner);
+printf("Name: %s\n", d->name);
+printf("Age: %f\n", d->age);
+printf("Owner: %s\n", d->owner);
 }
 }
