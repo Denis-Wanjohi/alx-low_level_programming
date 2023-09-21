62d2044a92b157f0fb67edc9b6989dc1e73b170d
gightkingss <gightkingss@gmail.com>
2023-09-21 01:29:39 -0700

    printin the list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "list.h"
+
+size_t print_list(const list_t *h) 
+{
+const list_t *current = h;
+int x = 0;
+while (current != NULL)
+{
+if ((current->str) == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%d] ", current->len);
+printf("%s\n", current->str);        
+}
+x++;
+current = current->next;
+}   
+return x;
+}
