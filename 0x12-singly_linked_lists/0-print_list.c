921c07d185343626a4edbea4cd0360b292378763
gightkingss <gightkingss@gmail.com>
2023-09-21 01:49:40 -0700

    list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,10 @@
 #include "list.h"
+/**
+ * print_list(const list_t *h)-prints the content
+ * typedef list_t
+ * @h:pointer
+ * return- the total no of cotnt
+ */
 
 size_t print_list(const list_t *h) 
 {
