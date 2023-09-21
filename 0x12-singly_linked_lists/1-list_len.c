2fe23d4c72a352ea667c0e3c1f5b36e259f02255
gightkingss <gightkingss@gmail.com>
2023-09-21 01:45:18 -0700

    list length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "list.h"
+/**
+ * print_list(const list_t *h)-checks the list length
+ * typedef list_t
+ * @h:pointer
+ * return-the length
+ */
+size_t print_list(const list_t *h) 
+{
+const list_t *current = h;
+int x = 0;
+while (current != NULL)
+{
+x++;
+current = current->next;
+}
+return x;
+}
