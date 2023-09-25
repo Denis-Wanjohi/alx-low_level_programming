ccc9fe6db96ad9e0275d2e68d72cb4eb786c5656
gightkingss <gightkingss@gmail.com>
2023-09-25 04:20:09 -0700

    list the length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * listint_len(const listint_t *h)-checks the no of items
+ * typedef listint_t
+ * @h:pointer
+ * Return-number of items 
+*/
+size_t listint_len(const listint_t *h)
+{
+int counter = 0;
+while (h != NULL)
+{
+h = h->next;
+counter++;
+}
+return counter;
+}
