6bfafc37252e9ed353f1fa492e1c6399ab786e3a
gightkingss <gightkingss@gmail.com>
2023-09-25 22:37:09 -0700

    addding node at the end

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * generateNewNode()-creates a new node
+ * @n:the node
+ * Return-the new created node
+*/
+listint_t *generateNewNode(int n)
+{
+listint_t *newNode = (listint_t*)malloc(sizeof(listint_t));
+newNode->n = n;
+newNode->next = NULL;
+return newNode;
+}
+/**
+ * listint_t *add_nodeint_end(listint_t **head, const int n)-function adding node
+ * typedef listint_t
+ * @head:pointer to struct
+ * @n:the vale
+ * Return:the address
+ * 
+*/
+listint_t *add_nodeint_end(listint_t **head, const int n){
+listint_t *newNode = generateNewNode(n);
+if(*head == NULL)
+{
+*head = newNode;
+}
+else
+{
+listint_t *now = *head;
+while (now->next !=NULL)
+{
+now = now->next;
+}
+now->next = newNode;
+}
+return newNode;
+}
