e39865b12c72d2129d88ea6a051a855e441f9ddc
gightkingss <gightkingss@gmail.com>
2023-09-28 14:50:51 -0700

    binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * binary_to_uint - convert to int
+ * @b: value being used
+ * Return: the int value
+ */
 
 unsigned int binary_to_uint(const char *b)
 {
@@ -6,6 +11,10 @@ unsigned int result = 0;
 int f = 0;
 int length = 0;
 int i;
+if (b == NULL )
+{
+return (0);
+}
 while (b[f] != '\0')
 {
 length++;
