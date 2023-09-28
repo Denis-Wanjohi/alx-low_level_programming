261b1a3340f2df97603e5788b39f827b3dc307f0
gightkingss <gightkingss@gmail.com>
2023-09-28 15:51:56 -0700

    int to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,18 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_binary - prints binary value 
+ * print_binary - prints binary value
  * @n: value being used
  */
 void print_binary(unsigned long int n)
 {
 int i, count = 0;
 	unsigned long int current;
-
 	for (i = 63; i >= 0; i--)
 	{
 		current = n >> i;
-
 		if (current & 1)
 		{
 			_putchar('1');
