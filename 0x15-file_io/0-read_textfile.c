d414aed939a7a79edbf041dc892df84ca5f07a9e
gightkingss <gightkingss@gmail.com>
2023-10-02 07:32:53 -0700

    read file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,43 +1,29 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-#include <unistd.h>
-#include "main.h"    
+
+/**
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
-{if (filename == NULL )
-	{
+{
+	char *buffer;
+	ssize_t fileread;
+	ssize_t writer;
+	ssize_t t;
 
+	fileread = open(filename, O_RDONLY);
+	if (fileread == -1)
 		return (0);
-	}
-FILE *file = fopen(filename, "r");
-if (file == NULL )
-{
-return (0);
+	buffer = malloc(sizeof(char) * letters);
+	t = read(fd, buffer, letters);
+	writer = write(STDOUT_FILENO, buffer, t);
 
-}
-char *buffer = (char  *)malloc(letters);
-
-if (buffer == NULL )
-{
-	fclose(file);
-	return (0);
-}
-size_t bytesRead = fread(buffer,1,lettres, file);
-if (bytesRead== 0)
-{
 	free(buffer);
-	fclose(file);
-	return (0);
+	close(fileread);
+	return (writer);
 }
-size_t bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
-if (bytesWritten != bytesRead)
-{
-	free(buffer);
-	fclose(file);
-	return (0);
-}
-free(buffer);
-fclose(file);
-return bytesRead;
 
-
-}
