5c61bde1cdc57cce2ecd75f96bcd4b2b0587b29d
gightkingss <gightkingss@gmail.com>
2023-10-02 07:11:47 -0700

    reading file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+    
+ssize_t read_textfile(const char *filename, size_t letters)
+{if (filename == NULL )
+	{
+
+		return (0);
+	}
+FILE *file = fopen(filename, "r");
+if (file == NULL )
+{
+return (0);
+
+}
+char *buffer = (char  *)malloc(letters);
+
+if (buffer == NULL )
+{
+	fclose(file);
+	return (0);
+}
+size_t bytesRead = fread(buffer,1,lettres, file);
+if (bytesRead== 0)
+{
+	free(buffer);
+	fclose(file);
+	return (0);
+}
+size_t bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
+if (bytesWritten != bytesRead)
+{
+	free(buffer);
+	fclose(file);
+	return (0);
+}
+free(buffer);
+fclose(file);
+return bytesRead;
+
+
+}
