d72296a9d93b846834fa5a5555b0199c3f0b06a1
gightkingss <gightkingss@gmail.com>
2023-10-03 05:13:50 +0300

    ceating a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int create_file(const char *filename, char *text_content)
+{
+    int len = 0;
+    if (filename == NULL) {
+        return (-1);
+    }
+
+    int filed = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+    if (filed == -1)
+    {
+        return (-1);
+    }
+
+    if (text_content != NULL)
+    {
+        for (len = 0; text_content[len];)
+			len++;
+	ssize_t bytes_written = write(filed, text_content,len));
+        close(filed);
+
+        if (bytes_written == -1)
+	{
+            return (-1);
+        }
+    }
+    else
+    {
+        close(fd);
+    }
+
+    return (1);
+}
+
