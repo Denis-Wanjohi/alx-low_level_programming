8d10649697d188735944010e67301013442476f1
gightkingss <gightkingss@gmail.com>
2023-10-26 06:45:04 -0700

    updates

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/*
+ * binary_to_unit - converts from binary
+ * @x: string to convert
+ * Return: the integer in base 10
+ */
+
+int binary_to_uint (char *x)
+{
+
+  int lenght = strlen (x);
+
+  int total = 0;
+
+  int position = 0;
+
+  int i;
+
+  for (i = lenght - 1; i >= 0; i--)
+    {
+
+      if (x[i] == '1')
+
+	total = total + (1 << position);
+
+      position++;
+
+    }
+  return total;
+
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#ifdef main.h
+#define main.h
+
+
+#include "stdio.h"
+#include "string.h"
+
+
+int binary_to_uint(char *x);
+
+
+#endif
