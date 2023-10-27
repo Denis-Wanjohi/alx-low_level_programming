9b88349e9c6811fb9b146c362a833c6614ee5bfd
gightkingss <gightkingss@gmail.com>
2023-10-26 19:43:38 -0700

    update

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,11 +1,6 @@
 #ifdef main.h
 #define main.h
 
-
-#include "stdio.h"
-#include "string.h"
-
-
 unsigned int binary_to_uint(char *x);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -13,7 +8,5 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
-
-
 
 #endif
